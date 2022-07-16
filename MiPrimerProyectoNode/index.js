10bba30b476ac88d39dc40c2d88601ab03352da6
Alejandro <molina.ramos@hotmail.com>
2022-07-16 13:07:45 +0100

    first commit

--- MiPrimerProyectoNode/index.js
+++ MiPrimerProyectoNode/index.js
@@ -1,3 +1,10 @@
 'use strict';
 
+//Este es el mensaje de bienvenida
+console.log('Esta es la puerta de entrada al proyecto')
+
+
+
+//Este es el mensaje que se envia para despedir
+console.log('Adios');
 
