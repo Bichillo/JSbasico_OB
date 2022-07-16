f1cd2be2f3104bb5eeffab3b8203d32c884f5a8b
Alejandro <molina.ramos@hotmail.com>
2022-07-16 13:42:01 +0100

    second

---
+++ Tema2/iniciacionTiposJS.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//tipos primitivos
+
+//number
+4;
+0;
+
+//string
+("hola");
+
+//booleans
+true;
+false;
+
+//nulo y undefined
+null;
+undefined;
+
+//null, undefined, false,0 ==> todas son false
+console.log(null === undefined);
+
+if (true) {
+  console.log("cumple");
+} else {
+  console.log("No cumple");
+}
---
+++ Tema2/iniciacionVariables.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var variable;
+let variableLet;
+//const constante;
+const constante = "Hola soy una constante";
+console.log(constante);
+
+var a = 1;
+console.log(a);
+
+a = 4;
+console.log(a);
+
+let b = 3;
+console.log(b);
+
+b = 5;
+console.log(b);
+
+console.log(typeof b);
+console.log(typeof constante);
