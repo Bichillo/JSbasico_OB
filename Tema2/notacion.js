3aa52970c1edcd869a73c23b23d6d0f89c3f99fc
Alejandro <molina.ramos@hotmail.com>
2022-07-16 13:56:58 +0100

    añado notacion.js

---
+++ Tema2/notacion.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//Notacion
+// ; -> Delimita el final de una linea
+// . -> Se utiliza en los objetos para acceder a los atrributos.
+//movil.anchura
+
+// [] -> Se utiliza para listas, arreglos o arrays.
+const ar = [1, 2, 3, 4];
+
+// () -> Se utiliza para las funciones
+function suma(a, b) {
+  //se escribe la funcion
+}
+
+// {} -> Se utiliza para obketos, funciones y estructuras de control
+const movil = {
+  anchura: 5,
+  altura: 10,
+};
+
+if (true) {
+  //todo lo que haya entre llaves
+
+  const constante2 = "hola";
+}
