44dc0ca74882c7d935740598835b47ddb0a01c6a
Alejandro <molina.ramos@hotmail.com>
2022-07-18 17:18:37 +0100

    añado tema3

---
+++ Tema 3/if-else-basico.js
@@ -0,0 +1,22 @@
+"use strict";
+//Estructuras de control:
+//Metodologia que nos permite manejar, y tener control de nuestro codigo,
+//Podemos dirigir nuestro codigo por donde nos interese en funcionn de nuestras condiciones,
+//se utiliza bifurcaciones y bucles.
+
+//Bifurcaciones: nos permiten decidir seguir por uno u otro camino.
+//if-else, comparaciones y switch.
+
+//if-else:
+let saldo = 50;
+let efectivo = 100;
+
+if (efectivo < saldo) {
+  console.log("Puedes sacar dinero");
+}
+
+if (efectivo < saldo) {
+  console.log("Puedes sacar dinero");
+} else {
+  console.log("Saldo insuficiente");
+}
---
+++ Tema 3/ifElse-elseif.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//if else + if else
+
+let nota = 5;
+
+if (nota === 5) {
+  console.log("Enhorabuena has aprobado");
+} else if (nota === 4) {
+  console.log("Has suspendido, podrias haberlo hecho mejor");
+} else if (nota === 3) {
+  console.log("Necesitas mejorar");
+} else if (nota === 2) {
+  console.log("Esfuerzate mas");
+} else if (nota === 1) {
+  console.log("No has estudiado nada");
+} else {
+  console.log("ERROR, introduce unaa nota entre el 1 y el 5");
+}
