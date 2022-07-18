17b1c03680c79c5f81f2fd97f223b0e580b0f45e
Alejandro <molina.ramos@hotmail.com>
2022-07-18 16:12:56 +0100

    agrego ejercicio

---
+++ Tema2/ejercicio/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const miLista = {
+  nombre: "Alejandro",
+  edad: 36,
+  desarrolador: false,
+  fechaNacimiento: Date(10 / 5 / 1986),
+  libro: {
+    titulo: "La ultima Legion",
+    autor: "No me acuerdo",
+    fecha: Date(10 / 05 / 1999),
+    url: "NoTengoNiIdea.com",
+  },
+};
+console.log(miLista);
